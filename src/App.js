bbfdfb725cfa522c919fa08caef07833b349f18e
srmakp <shs@rapidinnovation.dev>
2021-05-27 18:39:45 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      password: "",
+    };
+  }
+  valid() {
+    if (!this.state.name.includes("@") && this.state.password.length < 5) {
+      this.setState({
+        nameError: "Invalid Name ",
+        passwordError: "Password length should be more than 5",
+      });
+    } else if (!this.state.name.includes("@")) {
+      this.setState({
+        nameError: "Invalid Name ",
+      });
+    } else if (this.state.password.length < 5) {
+      this.setState({
+        passwordError: "Password length should be more than 5",
+      });
+    } else {
+      return true;
+    }
+  }
+  submit() {
+    this.setState({
+      nameError: "",
+      passwordError: "",
+    });
+    if (this.valid()) {
+      alert("form has been submited");
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Form Validation</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            this.setState({ name: event.target.value });
+          }}
+        />
+        <p style={{ color: "red", fontSize: "1em" }}>{this.state.nameError}</p>
+        <br></br>
+        <input
+          type="password"
+          onChange={(event) => {
+            this.setState({ password: event.target.value });
+          }}
+        />
+        <p style={{ color: "red", fontSize: "1em" }}>
+          {this.state.passwordError}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <br></br>
+        <button onClick={() => this.submit()}>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
