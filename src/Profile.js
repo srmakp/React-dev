aede4d7caef5e7502b588e9775bef10b99ea6f3e
srmakp <shs@rapidinnovation.dev>
2021-05-27 15:55:00 +0530

    React-componentDidUpdate

--- src/Profile.js
+++ src/Profile.js
@@ -1,22 +1,52 @@
 import React from "react";
 
+// export default class Profile extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       data: null,
+//     };
+//     console.warn("constructor");
+//   }
+//   componentDidMount() {
+//     this.setState({ data: "update" });
+//     console.warn("componentDidMount");
+//   }
+//   render() {
+//     console.warn("render");
+//     return (
+//       <div>
+//         <h1>Life Cycle Method : componentDidMount</h1>
+//       </div>
+//     );
+//   }
+// }
+
 export default class Profile extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: null,
+      active: null,
+      who: null,
     };
     console.warn("constructor");
   }
-  componentDidMount() {
-    this.setState({ data: "update" });
-    console.warn("componentDidMount");
+  componentDidUpdate() {
+    console.warn("componentDidUpdate");
+    if (this.state.who == null) this.setState({ who: "Prem Singh" });
   }
   render() {
     console.warn("render");
     return (
       <div>
-        <h1>Life Cycle Method : componentDidMount</h1>
+        <h1>Life Cycle Method : componentDidUpdate:: {this.state.who}</h1>
+        <button
+          onClick={() => {
+            this.setState({ active: "yes" });
+          }}
+        >
+          Click me
+        </button>
       </div>
     );
   }
