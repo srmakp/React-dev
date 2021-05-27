5dee9e1c07a9f89030c45abd49eb77f178eaf451
srmakp <shs@rapidinnovation.dev>
2021-05-27 12:51:53 +0530

    props implementation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Profile from "./Profile.js";
+import Props from "./Props";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Profile />
+        <Props text={{ name: "Prem singh" }} data="props Data" />
       </header>
     </div>
   );
---
+++ src/Props.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Props(prop) {
+  return (
+    <div>
+      <h1>{prop.text.name}</h1>
+      <h1>prop.data</h1>
+    </div>
+  );
+}
+export default Props;
