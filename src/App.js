ce0de441436d1e0259ed9e2b0cb3e3c2eb62ec0c
srmakp <shs@rapidinnovation.dev>
2021-05-28 12:23:42 +0530

    routing in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./cmp/Home";
+import Profile from "./cmp/Profile";
+import About from "./cmp/About";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Link to="">Home </Link>
+          <Link to="/about">About </Link>
+          <Link to="/profile">Profile </Link>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
+        </Router>
+
+        {/* <Profile/>
+   <Home/>
+   <About/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/cmp/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile component</h1>
+      </div>
+    );
+  }
+}
+export default Profile;
