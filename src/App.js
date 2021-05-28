fc628c687b4837d950c8aa5878c4f8edfafb6d6d
srmakp <shs@rapidinnovation.dev>
2021-05-28 14:30:08 +0530

    fetching Api-data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: null,
+    };
+  }
+  componentDidMount() {
+    fetch("https://reqres.in/api/users").then((resp) => {
+      resp.json().then((result) => {
+        console.warn(result.data);
+        this.setState({ users: result.data });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Fetch API Data</h1>
+        {this.state.users
+          ? this.state.users.map((item, i) => (
+              <div className="App">
+                <p>
+                  {i} ---
+                  {item.first_name}
+                  {item.last_name}--
+                  {item.email}
+                </p>
+              </div>
+            ))
+          : null}
+      </div>
+    );
+  }
 }
 
 export default App;
