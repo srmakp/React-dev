4f99aaf23d51a7ee80db86171199729fa3635a56
srmakp <shs@rapidinnovation.dev>
2021-05-28 16:35:44 +0530

    implementation in modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+  }
+  handleModal() {
+    this.setState({ show: !this.state.show });
+  }
+  render() {
+    return (
+      <div>
+        <Button onClick={() => this.handleModal()}>Click Me</Button>
+        <Modal show={this.state.show} onHide={() => this.handleModal()}>
+          <Modal.Header closeButton>Model Header</Modal.Header>
+          <Modal.Body>Hello Rapid Innovation(CodeZero2pi</Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => this.handleModal()}>Close</Button>
+            <Button onClick={() => this.handleModal()}>save</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
